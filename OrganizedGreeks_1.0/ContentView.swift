f31ca103cbe47883223fe1ba8c64ac9d3fe9bd7e
Brennan Rivera <brennanrivera0@gmail.com>
2023-08-20 21:48:40 -0400

    Initial Commit

---
+++ OrganizedGreeks_1.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OrganizedGreeks_1.0
+//
+//  Created by Brennan Rivera on 8/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OrganizedGreeks_1.0/OrganizedGreeks_1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizedGreeks_1_0App.swift
+//  OrganizedGreeks_1.0
+//
+//  Created by Brennan Rivera on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct OrganizedGreeks_1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
